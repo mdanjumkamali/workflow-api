807a731a1d1eaf87f14cfffa3b1a9838c88610af
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-07-27 14:32:24 +0530

    Add Task Model

---
+++ models/task/task.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["To-Do", "In Progress", "Under Review", "Completed"],
+      required: true,
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "Urgent"],
+    },
+    deadline: {
+      type: Date,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Task = mongoose.model("Task", taskSchema);
