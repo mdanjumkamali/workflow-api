38fdb6845e58491a8818a7417330bb67e6459f8b
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-07-27 14:52:28 +0530

    Add Auth Middleware

---
+++ middlewares/auth.middleware.ts
@@ -0,0 +1,55 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models/user/user.model";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+      };
+    }
+  }
+}
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token =
+      req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Authorization token not provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+    if (typeof decoded !== "object" || !decoded.id) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = {
+      id: user.id,
+      email: user.email,
+    };
+
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
